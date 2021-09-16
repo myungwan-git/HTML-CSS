af1ab927a0f0301464d6595c8200c6d350124ebd
myungwan-git <87704319+myungwan-git@users.noreply.github.com>
2021-09-16 20:52:56 +0900

    20210916 HTML-CSS JS function

--- main.js
+++ main.js
@@ -10,3 +10,37 @@ document.addEventListener('scroll', () => {
         navbar.classList.remove('navbar--dark');
     }
 });
+
+//Handle scrolling
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click', (event) => {
+    const target = event.target;
+    const link = target.dataset.link;
+    if(link==null) {
+        return;
+    }
+    scrollIntoView(link);
+});
+
+const homeContactBtn = document.querySelector('.home__contact');
+homeContactBtn.addEventListener('click', (event) => {
+    scrollIntoView('#contact');
+});
+
+
+//opacity
+const home = document.querySelector('#home');
+const homeHeight = home.getBoundingClientRect().height;
+document.addEventListener('scroll', ()=> {
+    const opa = 1 - window.scrollY / homeHeight;
+    home.style.opacity = opa;
+});
+
+
+
+
+function scrollIntoView(selector) {
+    const scrollTo = document.querySelector(selector);
+    scrollTo.scrollIntoView({behavior: 'smooth'});   
+}
+
